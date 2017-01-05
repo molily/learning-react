dfab7313f4ad1a75ff30b14fb2a07ad194f127ec
Mathias Schaefer (molily) <zapperlott@gmail.com>
2017-01-05 11:16:49 +0100

    Improves comment

--- 4-counter4/src/Counter.js
+++ 4-counter4/src/Counter.js
@@ -5,7 +5,8 @@ export default class Counter extends PureComponent {
   // Performance optimization:
   // Implement a custom shouldComponentUpdate function. With each render of
   // the parent component, Counter gets new on* callback functions.
-  // This would lead to a re-render even if the count did not change.
+  // PureComponent’s shouldComponentUpdate method would detect this change and
+  // trigger a re-rendering even though the count did not change.
   shouldComponentUpdate(nextProps) {
     // Has the count changed? If not, there is no need to re-render.
     return nextProps.count !== this.props.count;
